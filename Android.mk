d30afb971dbac8f49cf0b438572832d56cc32d12
Reza Mohammadi <reza@farsitel.com>
2011-07-02 11:49:32 +0430

    Duplicated libadad, renaming to have an independent package which can also be built beside AthanTime

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := libadad
+LOCAL_STATIC_JAVA_LIBRARIES := libadad_qiblacompass
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -32,7 +32,7 @@ include $(BUILD_PACKAGE)
 ##################################################
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libadad:lib/adad.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libadad_qiblacompass:lib/adad.jar
 include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
